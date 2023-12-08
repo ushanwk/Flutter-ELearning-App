0bfad09866bdfce293e138adfa09795349196422
UshanKaushalya <Ushanwk22@gmail.com>
2023-12-08 21:34:35 +0530

    Home page app bar done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,24 +10,79 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      onPressed: (){},
+                      icon: const Icon(
+                          Icons.dashboard_customize_rounded,
+                        color: kblue,
+                      ),
+                  ),
+                  IconButton(
                     onPressed: (){},
                     icon: const Icon(
-                        Icons.dashboard_customize_rounded,
-                      color: kblue,
+                      Icons.search_rounded,
                     ),
-                ),
-                IconButton(
-                  onPressed: (){},
-                  icon: const Icon(
-                    Icons.search_rounded,
-                  ),
-                )
-              ],
+                  )
+                ],
+              ),
             ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Hi Ushan',
+                            style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            'Today is a Good day\nto learn something new !',
+                            style: TextStyle(
+                                color: Colors.black54,
+                                height: 1.5,
+                                fontSize: 13,
+                                fontWeight: FontWeight.w700
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Spacer(),
+                      Column(
+                        children: [
+                          Container(
+                            height: 70,
+                            width: 70,
+                            decoration: BoxDecoration(
+                              color: kpurple,
+                              borderRadius: BorderRadius.circular(15)
+                            ),
+                            child: Image.asset('assest/image/profile.png'),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       )
